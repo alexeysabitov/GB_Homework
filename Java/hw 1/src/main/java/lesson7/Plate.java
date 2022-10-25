7e3126f00fcb18223efe76c0b89452f451162dc2
Alexey.Sabitov <lexx.biysk@mail.ru>
2022-10-25 20:45:52 +0700

    lesson7

---
+++ Java/hw 1/src/main/java/lesson7/Plate.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+public class Plate {
+    public int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+
+    public void increaseFood(int x) {
+        food += x;
+    }
+
+    public void printInfo() {
+        System.out.println("Корма в тарелке: " + food + " грамм");
+    }
+}
