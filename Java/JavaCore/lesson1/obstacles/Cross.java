2df4b3419316dc07975ce8b611b228e1915f621e
Alexey Sabitov <lexx.biysk@mail.ru>
2022-11-13 20:42:36 +0700

    lesson1

---
+++ Java/JavaCore/lesson1/obstacles/Cross.java
@@ -0,0 +1,17 @@
+package JavaCore.lesson1.obstacles;
+
+import JavaCore.lesson1.Participant;
+
+public class Cross extends Obstacle {
+
+    private int length;
+
+    public Cross(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void doIt(Participant animal) {
+        animal.run(length);
+    }
+}
