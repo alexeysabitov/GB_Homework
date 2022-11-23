285d347b5071252bc770b66fc9a067bfd3ed021d
Alexey Sabitov <lexx.biysk@mail.ru>
2022-11-23 23:23:22 +0700

    lesson3

---
+++ Java/JavaCore/lesson3/Apple.java
@@ -0,0 +1,13 @@
+package JavaCore.lesson3;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(100);
+    }
+
+    @Override
+    public String toString() {
+        return "яблоки";
+    }
+}
